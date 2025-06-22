57c801fbf824bb9ababdfe4d724a4a8647828bc7
Parth Rathod <rathodparth02@gmail.com>
2025-06-22 15:12:08 -0500

    add doc strings

--- insight/pypi/utils.py
+++ insight/pypi/utils.py
@@ -10,6 +10,19 @@ RSS_FEED_URL = "https://pypi.org/rss/updates.xml"
 
 
 def get_request(url: str) -> requests.Response | None:
+    """
+    Sends a GET request to the specified URL and returns the response if successful.
+
+    Args:
+        url (str): The URL to send the request to.
+
+    Returns:
+        requests.Response | None: The response object if the request was successful, otherwise None.
+
+    Raises:
+        Exception: If the request fails and the status code is not 200.
+    """
+
     response = requests.get(url)
     if response.status_code == 200:
         return response
@@ -20,6 +33,19 @@ def get_request(url: str) -> requests.Response | None:
 
 
 def get_pypi_packages_uploaded_today() -> list[RecentPackages]:
+    """
+    Retrieves recently uploaded PyPI packages from the RSS feed.
+
+    This function parses the RSS feed, extracts relevant information about newly uploaded packages,
+    and returns them as a list of RecentPackages.
+
+    Returns:
+        list[RecentPackages]: A list of Recently Uploaded Packages objects containing information about newly uploaded packages.
+
+    Note:
+        This function relies on the PyPI RSS feed being available and correctly formatted.
+    """
+
     data = []
     response = get_request(RSS_FEED_URL)
     root = ET.fromstring(response.content)
@@ -50,6 +76,20 @@ def get_pypi_packages_uploaded_today() -> list[RecentPackages]:
 
 
 def get_github_link(package_name: str, package_version: str) -> str | None:
+    """
+    Attempts to retrieve the GitHub link for a given PyPI package version.
+
+    Args:
+        package_name (str): The name of the PyPI package.
+        package_version (str): The version of the PyPI package.
+
+    Returns:
+        str | None: The GitHub link if found, otherwise None.
+
+    Note:
+        This function relies on the PyPI JSON API being available and correctly formatted.
+    """
+
     url = f"https://pypi.org/pypi/{package_name}/{package_version}/json"
     response = requests.get(url)
 
@@ -60,9 +100,3 @@ def get_github_link(package_name: str, package_version: str) -> str | None:
         if package_info["project_urls"].get("Homepage"):
             return package_info["project_urls"].get("Homepage")
     return None
-
-
-# CLI Testing Purpose Only
-if __name__ == "__main__":
-    data = get_pypi_packages_uploaded_today()
-    print(data)
