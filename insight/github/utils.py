1b607d62e48a4583745ed7575f39197e0e647b7f
Parth Rathod <rathodparth02@gmail.com>
2025-06-25 17:50:31 -0500

    github table

---
+++ insight/github/utils.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+from insight.pypi.utils import get_request
+from insight.github.datatypes import GithubInfo
+
+
+def get_repo_info(repo_url: str) -> GithubInfo:
+    """
+    Fetches and returns information about a GitHub repository.
+
+    Args:
+        repo_url (str): The URL of the GitHub repository.
+
+    Returns:
+        GithubInfo: An object containing detailed information about the repository.
+
+    Raises:
+        ValueError: If the provided URL is invalid.
+    """
+
+    parts = repo_url.rstrip("/").split("/")
+    if len(parts) < 2:
+        raise ValueError("Invalid GitHub URL")
+    owner, repo = parts[-2], parts[-1]
+
+    # GitHub API endpoint
+    api_url = f"https://api.github.com/repos/{owner}/{repo}"
+
+    response = get_request(api_url)
+
+    data = response.json()
+
+    created_date_iso = datetime.strptime(data.get("created_at"), "%Y-%m-%dT%H:%M:%SZ")
+    created_date = created_date_iso.strftime("%Y-%m-%d")
+
+    last_updated_iso = datetime.strptime(data.get("updated_at"), "%Y-%m-%dT%H:%M:%SZ")
+    last_updated_date = last_updated_iso.strftime("%Y-%m-%d")
+
+    return GithubInfo(
+        github_link=repo_url,
+        stars=data.get("stargazers_count", "0"),
+        watchers=data.get("subscribers_count", "0"),
+        forks=data.get("forks_count", "0"),
+        open_issues=data.get("open_issues_count", "0"),
+        created_date=created_date,
+        last_updated_date=last_updated_date,
+        license=data.get("license").get("name", None) if data.get("license") else None,
+    )
--- insight/main.py
+++ insight/main.py
@@ -5,7 +5,11 @@ from typing import Literal
 from insight.config import load_config, PACKAGE_VERSION
 from insight.pypi.utils import get_pypi_packages_uploaded_today
 from insight.db.main import DatabaseEntries
-from insight.db.utils import insert_into_pypi_packages_table
+from insight.db.insert import (
+    insert_into_pypi_packages_table,
+    insert_into_github_info_table,
+)
+from insight.db.select import github_repos_metadata
 from insight.decorator.main import timing_decorator
 
 logger = logging.getLogger(__name__)
@@ -38,3 +42,33 @@ def ingest_pypi(mode: Literal["dev", "prod"]) -> bool:
         pypi_ingest_status = False
 
     return pypi_ingest_status
+
+
+@timing_decorator
+def post_to_reddit(mode: Literal["dev", "prod"]) -> bool:
+    try:
+        post_reddit_status = False
+
+        logger.info("=" * 60)
+        logger.info(f"🚀 Run started at {datetime.now():%Y-%m-%d %H:%M:%S}")
+        logger.info(f"Package Running in {mode!r} Mode")
+        logger.info(f"Package Version:- {PACKAGE_VERSION!r}")
+        logger.info("=" * 60)
+
+        config = load_config(section=mode)
+
+        db = DatabaseEntries(config=config)
+        conn = db.get_connection_object
+
+        data = github_repos_metadata(conn=conn)
+
+        insert_into_github_info_table(conn=conn, data=data)
+
+        db.shutdown()
+
+        post_reddit_status = True
+    except Exception:
+        logger.critical("Reddit Post Failed", exc_info=True)
+        post_reddit_status = False
+
+    return post_reddit_status
