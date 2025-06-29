e4ff19000c08519d9524af6a2f9cafaede7695a4
Parth Rathod <rathodparth02@gmail.com>
2025-06-29 10:25:03 -0500

    correct timze format

--- insight/log/handler.py
+++ insight/log/handler.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime
 from pathlib import Path
 
-LOG_FILE_NAME = f'{datetime.now().strftime("%d_%B_%Y")}.log'
+LOG_FILE_NAME = f'{datetime.now().strftime("%d_%B_%Y_%H:%M:%S")}.log'
 
 
 class FileHandlerLogger(logging.Handler):
